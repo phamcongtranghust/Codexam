ba834148e239429128ff9111d6170be2f5ad58ef
phamcongtranghust <85822726+phamcongtranghust@users.noreply.github.com>
2021-06-14 08:04:52 +0700

    Update Bai4_3.c

--- Bai4_3.c
+++ Bai4_3.c
@@ -3,13 +3,16 @@ int main()
 {
 int n,s=0;
 scanf("%d",&n);
+if(n>=1000||n<0)
+{
+	printf("ERROR");
+	return 0;
+} 
 while(n>0)
 {
 	s+=n%10;
 	n/=10;
 }
-if(n>=1000||n<0) printf("ERROR");
-else printf("%d",s);
+printf("%d",s);
 return 0;
 }
-
